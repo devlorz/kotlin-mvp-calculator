6a7a043e06058d6e8bbff5a08bebeeffa752d321
Nutti Saelor <leelorz6@gmail.com>
2017-08-01 20:58:25 +0700

    add more main activity test

--- app/src/test/java/com/leeway/android_kotlin_mvp/ui/main/MainActivityTest.kt
+++ app/src/test/java/com/leeway/android_kotlin_mvp/ui/main/MainActivityTest.kt
@@ -1,6 +1,9 @@
 package com.leeway.android_kotlin_mvp.ui.main
 
+import android.widget.Button
 import com.leeway.android_kotlin_mvp.BuildConfig
+import com.leeway.android_kotlin_mvp.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -42,6 +45,119 @@ class MainActivityTest {
     @Test
     @Throws(Exception::class)
     fun testOnCreate() {
+        assertThat(activity.mainPresenter, `is`(notNullValue()))
         verify(activity.mainPresenter).onAttach(activity)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testHaveButtons() {
+        assertThat(activity.btnCalC , `is`(notNullValue()))
+        assertThat(activity.btnCalZero , `is`(notNullValue()))
+        assertThat(activity.btnCalTripleZero, `is`(notNullValue()))
+        assertThat(activity.btnCalOne , `is`(notNullValue()))
+        assertThat(activity.btnCalTwo , `is`(notNullValue()))
+        assertThat(activity.btnCalThree , `is`(notNullValue()))
+        assertThat(activity.btnCalFour , `is`(notNullValue()))
+        assertThat(activity.btnCalFive , `is`(notNullValue()))
+        assertThat(activity.btnCalSix , `is`(notNullValue()))
+        assertThat(activity.btnCalSeven , `is`(notNullValue()))
+        assertThat(activity.btnCalEight , `is`(notNullValue()))
+        assertThat(activity.btnCalNine , `is`(notNullValue()))
+        assertThat(activity.btnCalDot , `is`(notNullValue()))
+        assertThat(activity.btnCalDelete , `is`(notNullValue()))
+        assertThat(activity.btnCalPlus , `is`(notNullValue()))
+        assertThat(activity.btnCalMinus , `is`(notNullValue()))
+        assertThat(activity.btnCalMultiple , `is`(notNullValue()))
+        assertThat(activity.btnCalDivide , `is`(notNullValue()))
+        assertThat(activity.btnCalEqual , `is`(notNullValue()))
+        assertThat(activity.tvAmount, `is`(notNullValue()))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalZeroClick() {
+        activity.btnCalZero.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalZeroPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalOneClick() {
+        activity.btnCalOne.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalOnePress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalTwoClick() {
+        activity.btnCalTwo.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalTwoPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalThreeClick() {
+        activity.btnCalThree.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalThreePress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalFourClick() {
+        activity.btnCalFour.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalFourPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalFiveClick() {
+        activity.btnCalFive.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalFivePress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalSixClick() {
+        activity.btnCalSix.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalSixPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalSevenClick() {
+        activity.btnCalSeven.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalSevenPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalEightClick() {
+        activity.btnCalEight.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalEightPress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBtnCalNineClick() {
+        activity.btnCalNine.performClick()
+        val currentAmount = activity.tvAmount.text.toString()
+        verify(activity.mainPresenter).onCalNinePress(currentAmount)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnDestroy() {
+        activity = activityController.destroy().get()
+        verify(activity.mainPresenter).onDetach()
     }
 }
